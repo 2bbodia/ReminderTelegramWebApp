6fcd0e85de35c31d9982cfcb3384efc997896073
Bohdan <bodia0b@gmail.com>
2023-02-02 19:51:59 +0200

    - Added Telegram Web App api - Made template for routes - Added material ui

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,23 @@
+import {useEffect} from "react";
+import './app.css';
+import {
+    BrowserRouter,
+    Route,
+    Navigate,
+    Routes
+} from 'react-router-dom';
+
+
+const tg = window.Telegram.WebApp;
 export default function App() {
+    useEffect(() => {
+        tg.ready();
+    }, [])
     return (
-        <></>
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
     );
 }
